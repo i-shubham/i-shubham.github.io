6b569e32792b9215be337a25bc3586aea587112c
Shubham Mallick <shubham.mallick@salesforce.com>
2024-07-14 03:14:24 +0530

    updated

--- travel/js/script.js
+++ travel/js/script.js
@@ -1,56 +1,52 @@
-$( document ).ready(function() {
-    var w = window.innerWidth;
+$(document).ready(function () {
+  var w = window.innerWidth;
 
-    if(w > 767){
-        $('#menu-jk').scrollToFixed();
-    }else{
-       // $('#menu-jk').scrollToFixed();
-    }
+  if (w > 767) {
+    $("#menu-jk").scrollToFixed();
+  } else {
+    // $('#menu-jk').scrollToFixed();
+  }
+});
 
-
-
-})
-
-
-
-
-$( document ).ready(function() {
-
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        autoplay: true,
-        dots: true,
-        autoplayTimeout: 5000,
-        navText:['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
-        }
-    })
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    autoplay: true,
+    dots: true,
+    autoplayTimeout: 5000,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
 });
 
 function openPopup(imageElement) {
-        const imageSrc = imageElement.src;
-        const imageAlt = imageElement.alt;
+  const imageSrc = imageElement.src;
+  const imageAlt = imageElement.alt;
 
-        document.getElementById('popup').style.display = 'flex';
-        document.getElementById('popupImage').src = imageSrc;
-        document.getElementById('popupAltText').innerText = imageAlt;
-        document.getElementById('mainContainer').classList.add('blurred');
-    }
+  document.getElementById("popup").style.display = "flex";
+  document.getElementById("popupImage").src = imageSrc;
+  document.getElementById("popupAltText").innerText = imageAlt;
+  document.getElementById("mainContainer").classList.add("blurred");
+}
 
 function closePopup() {
-    document.getElementById('popup').style.display = 'none';
-    document.getElementById('popupImage').src = '';
-    document.getElementById('popupAltText').innerText = '';
-    document.getElementById('mainContainer').classList.remove('blurred');
+  document.getElementById("popup").style.display = "none";
+  document.getElementById("popupImage").src = "";
+  document.getElementById("popupAltText").innerText = "";
+  document.getElementById("mainContainer").classList.remove("blurred");
 }
